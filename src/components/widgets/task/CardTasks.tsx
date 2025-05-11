ee666010725631b3dc268fde8c70fb35b0140eaa
Muhamad Yusof <mhduusop@gmail.com>
2025-05-11 22:24:46 +0700

    feat: add card and swap button is active

---
+++ src/components/widgets/task/CardTasks.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+export default function CardTasks() {
+   return (
+      <Card className="w-full h-full max-w-[734px] max-h-[737px] px-[32px] py-[24px] overflow-hidden">
+         <CardHeader></CardHeader>
+         <CardContent className="h-full overflow-y-auto">
+
+         </CardContent>
+      </Card>
+   );
+}
